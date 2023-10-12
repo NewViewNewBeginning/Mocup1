1bd5cc98ec50266658d3ad822c15918ee17705c1
Miki Kiki <krawczykmichal1@gmail.com>
2023-10-12 10:32:21 +0100

    apply dark/light mode

--- WIT-JS/js/module/mode.js
+++ WIT-JS/js/module/mode.js
@@ -1,3 +1,12 @@
 export function setMode() {
-	console.log("mode");
+	const sunIcon = document.querySelector(".fa-sun");
+	const moonIcon = document.querySelector(".fa-moon");
+	const rootElement = document.documentElement;
+
+	sunIcon.addEventListener("click", () => {
+		rootElement.classList.add("light-mode");
+	});
+	moonIcon.addEventListener("click", () => {
+		rootElement.classList.remove("light-mode");
+	});
 }
