8ff1c69df898b1b4349c18ee1eb79445069bf481
Miki Kiki <krawczykmichal1@gmail.com>
2023-10-12 20:42:15 +0100

    validate and coment code for test

--- WIT-JS/js/app.js
+++ WIT-JS/js/app.js
@@ -1,9 +1,20 @@
+// Importing the functionality for the image showcase
 import { initShowcase } from "./module/showcase.js";
+
+// Importing the functionality for showing/hiding text
 import { showText } from "./module/showText.js";
+
+// Importing the functionality for toggling light/dark mode
 import { setMode } from "./module/mode.js";
 
+// Once the entire HTML document has been completely loaded and parsed, execute the following functions
 document.addEventListener("DOMContentLoaded", function () {
+	// Initialize the image showcase functionality
 	initShowcase();
+
+	// Initialize the show/hide text functionality
 	showText();
+
+	// Initialize the mode switching (light/dark) functionality
 	setMode();
 });
--- WIT-JS/js/module/mode.js
+++ WIT-JS/js/module/mode.js
@@ -1,11 +1,20 @@
+// Function to set the mode (light or dark) for the website
 export function setMode() {
+	// Grab the sun icon from the DOM, which will represent the light mode
 	const sunIcon = document.querySelector(".fa-sun");
+
+	// Grab the moon icon from the DOM, which will represent the dark mode
 	const moonIcon = document.querySelector(".fa-moon");
+
+	// Grab the root element (highest parent) of the DOM. This is where the light/dark mode class will be toggled
 	const rootElement = document.documentElement;
 
+	// Add an event listener to the sun icon. When it's clicked, the light mode will be activated
 	sunIcon.addEventListener("click", () => {
 		rootElement.classList.add("light-mode");
 	});
+
+	// Add an event listener to the moon icon. When it's clicked, the light mode will be deactivated (i.e., dark mode will be activated)
 	moonIcon.addEventListener("click", () => {
 		rootElement.classList.remove("light-mode");
 	});
