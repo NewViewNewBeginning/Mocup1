eedaa4614dbd6cd1a967df2e1157a3f2890d8c99
Miki Kiki <krawczykmichal1@gmail.com>
2023-10-12 12:38:01 +0100

    add show/hide text to event section text

--- WIT-JS/js/module/showText.js
+++ WIT-JS/js/module/showText.js
@@ -1,3 +1,37 @@
 export function showText() {
-	console.log("text");
+	const showHideTextLinks = document.querySelectorAll(".show-text");
+
+	showHideTextLinks.forEach(link => {
+		link.addEventListener("click", e => {
+			const textElement = e.target.previousElementSibling;
+			const fullText = textElement.getAttribute("data-full-text");
+			const currentText = textElement.textContent;
+
+			// If text is truncated
+			if (currentText.endsWith("...")) {
+				textElement.textContent = fullText;
+				link.textContent = "Show less";
+			} else {
+				textElement.textContent = truncateText(fullText);
+				link.textContent = "Click here for more informations";
+			}
+		});
+	});
+
+	initialTruncate();
+}
+
+function initialTruncate() {
+	const allTexts = document.querySelectorAll(".event-text");
+	allTexts.forEach(textElement => {
+		if (textElement.textContent.length > 100) {
+			// Store the full text in a custom attribute
+			textElement.setAttribute("data-full-text", textElement.textContent);
+			textElement.textContent = truncateText(textElement.textContent);
+		}
+	});
+}
+
+function truncateText(text) {
+	return text.slice(0, 100) + "...";
 }
