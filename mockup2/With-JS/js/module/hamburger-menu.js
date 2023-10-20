c7fe7399ef5c5724cee52f4d354ece01200ee960
Miki Kiki <krawczykmichal1@gmail.com>
2023-10-20 09:26:59 +0100

    add check for hamburger menu to show menu for desktop

--- mockup2/With-JS/js/module/hamburger-menu.js
+++ mockup2/With-JS/js/module/hamburger-menu.js
@@ -1,8 +1,24 @@
 export function hamMenu() {
 	const menuIcon = document.querySelector(".ham-icon");
 	const menu = document.querySelector("nav");
+
 	menuIcon.addEventListener("click", function () {
-		console.log("click");
 		menu.classList.toggle("show");
 	});
+
+	// Ensure menu is visible for screens wider than 768px
+	window.addEventListener("resize", function () {
+		if (window.innerWidth > 768) {
+			menu.classList.remove("show");
+			menu.style.display = "block";
+		} else {
+			menu.style.display = "";
+		}
+	});
+
+	// Initial check in case the function is loaded on a screen already wider than 768px
+	if (window.innerWidth > 768) {
+		menu.classList.remove("show");
+		menu.style.display = "block";
+	}
 }
