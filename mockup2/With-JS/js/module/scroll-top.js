3f64bdf0aa191cb55e9735d52bceb8dfaf64ee58
Miki Kiki <krawczykmichal1@gmail.com>
2023-10-20 09:18:11 +0100

    add scrolltop button with style and js

--- mockup2/With-JS/js/module/scroll-top.js
+++ mockup2/With-JS/js/module/scroll-top.js
@@ -1,3 +1,18 @@
 export function scrollTop() {
-	console.log("scrollTop()");
+	const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 200) {
+			scrollTopBtn.style.display = "block";
+		} else {
+			scrollTopBtn.style.display = "none";
+		}
+	});
+
+	scrollTopBtn.addEventListener("click", () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	});
 }
